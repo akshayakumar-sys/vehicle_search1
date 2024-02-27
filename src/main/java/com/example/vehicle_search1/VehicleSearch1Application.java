214418ef6c8b058e0a0b8d5d694a9d174fe9407a
akshayakumar-sys <akshaykumaryadahalli@gmai.com>
2024-02-28 00:28:39 +0530

    file initial setup

---
+++ src/main/java/com/example/vehicle_search1/VehicleSearch1Application.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_search1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSearch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSearch1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vehicle_search1/VehicleSearch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_search1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
